acd773aa371c30d7006968c1aba62ae7af5d360f
Mahmoud Abdelrahman <arch.mahmoud.ouf111@gmail.com>
2024-03-07 14:24:42 +0800

    ignored credentials again

--- notebooks/credentials.py
+++ notebooks/credentials.py
@@ -1,4 +1,4 @@
 keys = {
-    "els-apikey" : "<your elsevier personal token>", 
-    "els-inst-token" : "<your elsevier institute token>",
+    "els-apikey" : "f4af734d56a079da010b05f250d7c3b5", 
+    "els-inst-token" : "7dfb8b168bd5cae0e742f06bfd23fe97",
 }
