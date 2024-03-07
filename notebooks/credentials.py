0ab33a90bb01df924a788c092413303274d3ec91
Mahmoud Abdelrahman <arch.mahmoud.ouf111@gmail.com>
2024-03-07 16:04:24 +0800

    data analytics

--- notebooks/credentials.py
+++ notebooks/credentials.py
@@ -1,4 +1,5 @@
 keys = {
     "els-apikey" : "f4af734d56a079da010b05f250d7c3b5", 
     "els-inst-token" : "7dfb8b168bd5cae0e742f06bfd23fe97",
+    "openai-key" : "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"
 }
